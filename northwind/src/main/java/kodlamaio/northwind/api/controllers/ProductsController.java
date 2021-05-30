31c47e43fd9a1b4f3cb80898bcbd65f988521c25
Murat Topallar <topallarm@outlook.com>
2021-05-31 00:34:19 +0300

    30.05.2021 Pazar gün sonu

--- northwind/src/main/java/kodlamaio/northwind/api/controllers/ProductsController.java
+++ northwind/src/main/java/kodlamaio/northwind/api/controllers/ProductsController.java
@@ -14,6 +14,7 @@ import kodlamaio.northwind.business.abstracts.ProductService;
 import kodlamaio.northwind.core.utilities.results.DataResult;
 import kodlamaio.northwind.core.utilities.results.Result;
 import kodlamaio.northwind.entities.concretes.Product;
+import kodlamaio.northwind.entities.dtos.ProductWithCategoryDto;
 
 @RestController // sen bir controllersın
 @RequestMapping("/api/products") // hangi isteklere bu controller cevap verecek
@@ -74,6 +75,12 @@ public class ProductsController
 	public DataResult<List<Product>> getAllSorted()
 	{
 		return this.productService.getAllSorted();
+	}
+
+	@GetMapping("/getProductWithCategoryDetails")
+	public DataResult<List<ProductWithCategoryDto>> getProductWithCategoryDetails()
+	{
+		return this.productService.getProductWithCategoryDetails();
 	}
 }
 
--- northwind/src/main/java/kodlamaio/northwind/dataAccess/abstracts/ProductDao.java
+++ northwind/src/main/java/kodlamaio/northwind/dataAccess/abstracts/ProductDao.java
@@ -6,6 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 import kodlamaio.northwind.entities.concretes.Product;
+import kodlamaio.northwind.entities.dtos.ProductWithCategoryDto;
 
 public interface ProductDao extends JpaRepository<Product, Integer>
 {
@@ -32,4 +33,12 @@ public interface ProductDao extends JpaRepository<Product, Integer>
 	List<Product> getByNameAndCategory(String productName, int categoryId);
 
 	// select * from products where product_name=bişey and categoryId=bişey
+
+	@Query("Select new kodlamaio.northwind.entities.dtos.ProductWithCategoryDto(p.id, p.productName, c.categoryName) From Category c Inner Join c.products p")
+	List<ProductWithCategoryDto> getProductWithCategoryDetails();
+
+	// select p.productId, p.productName, c.categoryName from category c innerjoin
+	// product p on c.categoryId = p.categoryId
+	// önce ana tabloyu yaz jpql de category (one) products (many)
+	// tüm alanlar seçilecekse From, baze kolonlar seçilecekse Select From
 }
